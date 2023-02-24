bb4594887947afb81b44a07d67a49107f2ed4285
Ewelina Amrogowicz <ewelina.amrogowicz@gmail.com>
2023-02-24 20:46:12 +0000

    Markdown generated. Prompt and responses.

--- index.js
+++ index.js
@@ -1,21 +1,75 @@
+// fs is a Node standard library package for writing file
 const fs = require("fs");
-const path = require('path');
+// generate path to the file
+const path = require("path");
+// prompt for questions
 const inquirer = require("inquirer");
+// creates data to be written down by fs
 const generateMarkdown = require("./utils/generateMarkdown");
 
-// array of questions for user
-const questions = [
+// console.log(generateMarkdown({ title: "Ewelina" }));
 
-];
+// list of licenses
+// const gitHubLicenses = {
+//   mit: "https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt",
+// };
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "Write title of your project",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "Write project description",
+      name: "description",
+    },
+    {
+      type: "input",
+      message: "Write project installation",
+      name: "installation",
+    },
+    {
+      type: "input",
+      message: "Write project usage",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "Choose license",
+      choices: [
+        "N/A",
+        "MIT",
+        "GNU General Public License v3.0",
+        "Academic Free License v3.0",
+      ],
+      name: "license",
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+
+    const generateMD = generateMarkdown(answers);
+
+    fs.writeFile("test.txt", generateMD, (err) =>
+      err ? console.error(err) : console.log("Success!")
+    );
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,30 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  let licenseType;
+  if (data.license == "N/A") {
+    licenseType = `This project does NOT have a license.`;
+  } else {
+    licenseType = `This project is licensed under the terms of the ${data.license} license.`;
+  }
 
+  return `# ${data.title}
+    ## Description
+      ${data.description}
+    ## Installation
+      ${data.installation}
+    ## Usage
+      ${data.usage}
+    ## License
+      ${licenseType}.
+    ## Contributing
+      ${data.contributing}
+    ## Tests
+      ${data.tests}
+    ## Questions
+      ${data.questions}
 `;
 }
 
 module.exports = generateMarkdown;
+
+//   ## ${data.tableOfContent}
